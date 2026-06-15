49060db4c3cf57b4d7f5792bd5b3432caf2b6a13
mattiacallara <mattiacallara0@gmail.com>
2026-06-15 17:16:12 +0200

    feat: add ticket discount calculation

--- main.js
+++ main.js
@@ -10,4 +10,18 @@ button.addEventListener("click", (event) => {
     const age = (Number(userAge.value));
     
     console.log({km, age});
+    
+    const prezzoBase = km * 0.21;
+    console.log(prezzoBase);
+
+    let prezzoFinale = prezzoBase;
+
+    if(age < 18) {
+        prezzoFinale = prezzoBase * 0.80;
+    } else if (age > 65) {
+        prezzoFinale = prezzoBase * 0.6;
+    }
+    console.log(prezzoFinale.toFixed(2));
+    
+    result.textContent = `Il prezzo finale del tuo biglietto è: ${prezzoFinale.toFixed(2)} €`;
 })
