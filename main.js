b6b886191fb16c84db7a9d0a96bfc4006d0171ab
mattiacallara <mattiacallara0@gmail.com>
2026-06-15 17:10:32 +0200

    feat: select Dom elements

---
+++ main.js
@@ -0,0 +1,4 @@
+const userKm = document.querySelector("#userKm");
+const userAge = document.querySelector("#userAge");
+const button = document.querySelector("#button");
+const result = document.querySelector("#result");
