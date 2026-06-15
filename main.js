8548a5da41919a5dbeaae97b73bd8efbdc30d151
mattiacallara <mattiacallara0@gmail.com>
2026-06-15 17:14:35 +0200

    feat: log user inputs from form

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ const userKm = document.querySelector("#userKm");
 const userAge = document.querySelector("#userAge");
 const button = document.querySelector("#button");
 const result = document.querySelector("#result");
+
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const km = (Number(userKm.value));
+    const age = (Number(userAge.value));
+    
+    console.log({km, age});
+})
